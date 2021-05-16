f117e6803c7742a3f70a1dc6b3703fbcc1bca85b
Biswarup Bhattacharjee <74323889+biswa2210@users.noreply.github.com>
2021-05-16 21:09:53 +0530

    Update app.po.ts

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -4,6 +4,11 @@ export class AppPage {
   async navigateTo(): Promise<unknown> {
     return browser.get(browser.baseUrl);
   }
+  /*
+CREATED BY BISWARUP BHATTACHARJEE
+EMAIL    : bbiswa471@gmail.com
+PHONE NO : 6290272740
+*/
 
   async getTitleText(): Promise<string> {
     return element(by.css('app-root .content span')).getText();
