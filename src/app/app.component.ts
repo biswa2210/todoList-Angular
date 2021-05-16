5ad132960aa28963b8df0b89c858edbda84a69c9
Biswarup Bhattacharjee <74323889+biswa2210@users.noreply.github.com>
2021-05-16 21:18:07 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+/*
+CREATED BY BISWARUP BHATTACHARJEE
+EMAIL    : bbiswa471@gmail.com
+PHONE NO : 6290272740
+*/
 export class AppComponent {
   title = 'ToDo List';
   constructor() {
